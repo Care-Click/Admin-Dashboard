635cc4fc8f1fc216bcd85a3bd49daaa09084af42
aminelaarif <laarifamine5569@gmail.com>
2024-05-02 15:14:28 +0100

    added component for patient table

---
+++ src/components/Tables/Patient.tsx
@@ -0,0 +1,15 @@
+
+import TableTwo from './TableTwo'
+import DefaultLayout from '../../layout/DefaultLayout'
+
+function Patient() {
+  return (
+    
+  <DefaultLayout>
+    <TableTwo/>
+    </DefaultLayout>
+    
+  )
+}
+
+export default Patient
