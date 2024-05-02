dabb65dbc0e66260696104145b15ecd8a514d46e
aminelaarif <laarifamine5569@gmail.com>
2024-05-02 15:14:15 +0100

    created component for doctor table

---
+++ src/components/Tables/Doctor.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TableOne from './TableOne'
+import DefaultLayout from '../../layout/DefaultLayout'
+function Doctor() {
+  return (
+    <DefaultLayout>
+        <TableOne/>
+    </DefaultLayout>
+  )
+}
+
+export default Doctor
