214760ce275776ea18020ed40f84c20c677639e1
aminelaarif <laarifamine5569@gmail.com>
2024-05-02 15:17:06 +0100

    styling tweeks for table 3

--- src/components/Tables/TableThree.tsx
+++ src/components/Tables/TableThree.tsx
@@ -35,8 +35,11 @@ const [data,setData]=useState<unverifiedInfo[]>([])
     }
   }
   return (
+
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+     <h1  className='' >unverified doctors</h1> 
       <div className="max-w-full overflow-x-auto">
+        
         <table className="w-full table-auto">
           <thead>
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
