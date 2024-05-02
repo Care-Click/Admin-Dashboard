95d25d961447064e57f93cda8aeadc61243d3147
aminelaarif <laarifamine5569@gmail.com>
2024-05-02 15:19:42 +0100

    removed unused things

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -45,18 +45,9 @@ const ECommerce: React.FC = () => {
             <CardDataStats
               title={element.speciality}
               total={parseInt(element._count)}
-              rate="0.95%"
+              
             >
-              <svg
-                className="fill-primary dark:fill-white"
-                width="22"
-                height="18"
-                viewBox="0 0 22 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {/* SVG content */}
-              </svg>
+              
             </CardDataStats>
           </div>
         </React.Fragment>
